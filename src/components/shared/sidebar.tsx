89a760b29b286f1fa67814964881b22042c2b029
NIKHILENIGMA <nickharmalkar18@gmail.com>
2026-05-05 10:37:44 -0700

    feat: Update Sidebar component to include user role-based navigation and adjust layout

--- src/components/shared/sidebar.tsx
+++ src/components/shared/sidebar.tsx
@@ -1,7 +1,7 @@
+import { useUser } from '@clerk/clerk-react'
 import { ChartNoAxesColumn, LayoutDashboard, Users } from 'lucide-react'
 import type { FC } from 'react'
 import { IoAnalyticsOutline } from 'react-icons/io5'
-import { MdSportsCricket } from 'react-icons/md'
 import { Link, NavLink } from 'react-router'
 
 interface NavItem {
@@ -11,6 +11,11 @@ interface NavItem {
 }
 
 const NAV_LINKS: NavItem[] = [
+    {
+        to: '/admin',
+        icon: LayoutDashboard,
+        label: 'Admin Dashboard'
+    },
     {
         to: '/dashboard',
         icon: LayoutDashboard,
@@ -22,14 +27,14 @@ const NAV_LINKS: NavItem[] = [
         label: 'Leaderboard'
     },
     {
-        to: '/my-squad',
+        to: '/franchise',
         icon: Users,
-        label: 'My Squad'
+        label: 'Franchise'
     },
     {
         to: '/matches',
-        icon: MdSportsCricket,
-        label: 'Matches'
+        icon: Users,
+        label: 'Match Hub'
     },
     {
         to: '/analytics',
@@ -39,8 +44,10 @@ const NAV_LINKS: NavItem[] = [
 ]
 
 const Sidebar: FC = () => {
+    const { user } = useUser()
+    const isAdmin = user?.publicMetadata?.role === 'admin'
     return (
-        <aside className="hidden border-r-1 lg:w-[15%] h-screen bg-white sticky top-0 py-4 md:flex flex-col items-center justify-between z-50">
+        <aside className="hidden border-r-1 lg:w-[10%] h-screen bg-white sticky top-0 py-4 md:flex flex-col items-center justify-between z-50">
             <Link to="/">
                 <img
                     src="https://res.cloudinary.com/dynbvnhcc/image/upload/v1775299513/logo-fortune_qda5xe.png"
@@ -49,18 +56,20 @@ const Sidebar: FC = () => {
                 />
             </Link>
             <nav className="mt-10 flex flex-col items-center space-y-4 h-[80%] w-full px-2">
-                {NAV_LINKS.map((link: NavItem, idx: number) => (
-                    <NavLink
-                        key={link.to + idx}
-                        to={link.to}
-                        end={link.to === '/dashboard'}
-                        className={({ isActive }: { isActive: boolean }) =>
-                            `${isActive ? 'text-primary shadow-lg bg-white rounded-sm' : 'text-foreground/80'} w-full p-3 rounded-lg hover:bg-primary/20 transition-colors cursor-pointer flex items-center gap-3 justify-start text-start`
-                        }>
-                        {link.icon && <link.icon className="h-5 w-5 shrink-0" />}
-                        <span className="leading-none">{link.label}</span>
-                    </NavLink>
-                ))}
+                {NAV_LINKS.map((link: NavItem, idx: number) =>
+                    isAdmin || link.to !== '/admin' ? (
+                        <NavLink
+                            key={link.to + idx}
+                            to={link.to}
+                            end={link.to === '/dashboard'}
+                            className={({ isActive }: { isActive: boolean }) =>
+                                `${isActive ? 'text-primary shadow-lg bg-white rounded-sm' : 'text-foreground/80'} w-full p-3 rounded-lg hover:bg-primary/20 transition-colors cursor-pointer flex items-center gap-3 justify-start text-start`
+                            }>
+                            {link.icon && <link.icon className="h-5 w-5 shrink-0" />}
+                            <span className="leading-none">{link.label}</span>
+                        </NavLink>
+                    ) : null
+                )}
             </nav>
         </aside>
     )
