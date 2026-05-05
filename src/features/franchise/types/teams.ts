8f471edd2ea514f8dca2c1ccef6044359b4b3c28
NIKHILENIGMA <nickharmalkar18@gmail.com>
2026-05-05 10:38:12 -0700

    feat: Add Player and Teams type definitions for franchise management

---
+++ src/features/franchise/types/teams.ts
@@ -0,0 +1,5 @@
+export type Roles = 'All' | 'Batsman' | 'Bowler' | 'All-Rounder' | 'Wicket-Keeper'
+
+export type Teams = 'All' | 'CSK' | 'MI' | 'RCB' | 'KKR' | 'SRH' | 'DC' | 'PBKS' | 'RR' | 'GT' | 'LSG'
+
+export type Nationality = 'All' | 'Indians' | 'Overseas'
